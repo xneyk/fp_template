c0e3ac87d314c14f7cbdda4f9898f64988c0f8c9
xneyk <aguadenadaledgar@gmail.com>
2026-09-07 15:41:09 +0200

    not done patternmatching1

--- src/main/scala/intro/PatternMatching1.scala
+++ src/main/scala/intro/PatternMatching1.scala
@@ -99,7 +99,12 @@ object PatternMatching1 {
    *
    *         Hint: you can use if statements in pattern matching.
    */
-  def firstDivByX(xs: List[Int], n: Int): OptionalNum = ???
+  def firstDivByX(xs: List[Int], n: Int): OptionalNum = xs match {
+
+    case first :: tail if (first % n == 0) => first
+    case Nil => Nothing()
+    case first :: tail => firstDivByX(tail, n)
+  }
 
   /** Q4 (2p)
    * Implement this function that returns a list of only the even numbers.
@@ -108,5 +113,9 @@ object PatternMatching1 {
    * @param xs the list to process.
    * @return the list of all even numbers in xs.
    */
-  def onlyEvenNumbers(xs: List[OptionalNum]): List[Int] = ???
+  def onlyEvenNumbers(xs: List[OptionalNum]): List[Int] = xs match {
+    case first :: tail if (first % 2 == 0) => first :: onlyEvenNumbers(tail)
+    case Nil => Nil
+    case first :: tail => onlyEvenNumbers(tail) // non even numbers
+  }
 }
