33bfe7f57157bfa559c898da879b7980c14f5501
Casper van der Straaten <caspervdstraaten@gmail.com>
2026-09-07 16:19:29 +0200

    PatternMatching2 questions

--- src/main/scala/intro/PatternMatching2.scala
+++ src/main/scala/intro/PatternMatching2.scala
@@ -60,7 +60,10 @@ object PatternMatching2 {
      * turns into 
      * 		List("?gniod","ouy","era","woh",",ouy","yeH")
      */
-    def drunkWords(xs: List[String]) : List[String] = ???
+    def drunkWords(xs: List[String]) : List[String] = xs match{
+      case h :: t => drunkWords(t) :+ h.reverse
+      case Nil => Nil
+    }
 
 
     /** Q7 (3p)
@@ -75,7 +78,12 @@ object PatternMatching2 {
      * NB! Note that we don't provide a test case for this exercise.
      * You are encouraged to write your own tests
      */
-    def myForAll[A](xs : List[A], f: A => Boolean) : Boolean = ???
+    def myForAll[A](xs : List[A], f: A => Boolean) : Boolean =  xs match{
+      case h :: t if (f(h)) => myForAll(t,f)
+      case h :: t => false
+      case Nil => true
+    }
+
 
 
     /** Q8 (3p)
@@ -90,7 +98,11 @@ object PatternMatching2 {
      * 	lastElem(List()) // None
      * 	lastElem(List.range(0,3)) // Some(2) (range has exclusive ceiling)
      */
-    def lastElem[A](xs : List[A]) : Option[A] = ???
+    def lastElem[A](xs : List[A]) : Option[A] = xs match{
+      case h :: t if(lastElem(t) == None) => Some(h)
+      case h :: t => lastElem(t)
+      case Nil => None
+    }
 
     /** Q9 (4p)
      * Take two lists and concatenate them, returning the result
@@ -102,6 +114,9 @@ object PatternMatching2 {
      * 		append(List(), List()) 			// List()
      * 		append(List(1,3,5), List(2,4)) 	// List(1,3,5,2,4)
      */
-    def append[A](xs: List[A], ys: List[A]) : List[A] = ???
+    def append[A](xs: List[A], ys: List[A]) : List[A] = xs match {
+      case Nil => ys
+      case h ::t => h:: append(t, ys)
+    }
 
 }
